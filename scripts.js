1a102c67f0d31a3bead90585a6e3c00a34522a42
Thiago Marcelino | LAFORT <thiago.marcelino@lafortmalhas.local>
2025-08-07 16:55:25 -0300

    refactor: previnindo parra não ocorrer erro

--- scripts.js
+++ scripts.js
@@ -84,27 +84,25 @@ inputTags.addEventListener("keypress", async (evento) => {
 
 const botaoPublicar = document.querySelector("botao__publicar");
 
-botaoPublicar.addEventListener("click", async (evento) =>{
-    evento.preventDefault();
-
-    const nomeDoProjeto = document.getElementById("nome").value;
-    const descricaoDoProjeto = document.getElementById("descricao").value;
-    const tagsProjetos = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
-
-})
-
 async function publicarProjeto(nomeDoProjeto, descricaoDoProjeto, tagsProjetos){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             const deuCerto = Math.random() > 0.5;
 
             if(deuCerto){
-                resolve("Projeto publicado com sucesso.");
+                resolve("Projeto publicado com sucesso.")
             }else {
-                reject("Erro de publicação do projeto");
+                reject("Erro de publicação do projeto.")
             }
         }, 2000)
     })
 
 }
 
+botaoPublicar.addEventListener("click", async (evento) =>{
+    evento.preventDefault();
+
+    const nomeDoProjeto = document.getElementById("nome").value;
+    const descricaoDoProjeto = document.getElementById("descricao").value;
+    const tagsProjetos = Array.from(listaTags.querySelectorAll("p")).map((tag) => tag.textContent);
+})
